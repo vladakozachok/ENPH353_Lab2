28bceac0e178a6d5feebcb93077d00914c4e0eaa
vladakozachok <71842961+vladakozachok@users.noreply.github.com>
2023-02-01 23:46:45 -0800

    Update move_robot.py

--- enph353_ros_lab/node/move_robot.py
+++ enph353_ros_lab/node/move_robot.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+
 import rospy
 import cv2
 import numpy as np
@@ -7,8 +8,10 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 from simple_pid import PID
 
+## Class that subscribes to camera and moves the robot based on the PID controller.
+#
 class move_robot:
-
+    ## Constructor for the class
     def __init__(self):
         # Subscribe to the camera and initialize the velocity publisher and bridge
         self.bridge = CvBridge.CvBridge()
@@ -17,7 +20,9 @@ class move_robot:
         self.pid = PID(1, 0.2, 0.01, setpoint = 0)
         self.subscriber = rospy.Subscriber("/robot/camera1/image_raw", Image, self.callback)
 
-
+    ## Callback function for the subscriber.
+    #
+    #  @param data is the image data received by the subscriber.
     def callback(self, data):
         try:
             frame = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -25,11 +30,14 @@ class move_robot:
             twist.linear.x = 0.2
             twist.angular.z = self.pid(self.getPidError(frame))
             self.publisher.publish(twist)
-            
-            
         except self.CvBridgeError as e:
             print(e)
 
+    ## Function that returns the error for the PID controller.
+    #
+    #  @param img is the input image for the function.
+    #
+    #  @returns the error value for the PID controller.
     def getPidError(self,img):
         height, width, _ = img.shape
         imageCenter = width // 2
